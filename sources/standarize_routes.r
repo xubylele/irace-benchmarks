3422cf776308b26e794ee21542549e696a0b65d1
Xavier Diaz <diazxavier27gmail.com>
2020-11-17 14:13:18 -0300

    na filename

--- sources/standarize_routes.r
+++ sources/standarize_routes.r
@@ -34,67 +34,79 @@ standarize_routes <- function(){
 
     }
     
-    for(i in 8001:length(instances_filenames)){
+    for(i in 8130:length(instances_filenames)){
+        cat('\n')
+        print('--------------------------------------------------------------------------------')
         head_filename <- instances_filenames[i][[1]][[2]]
 
-        print(head_filename)
-        print(i)
+        if(!(is.na(head_filename) || head_filename == '')){
+            print(head_filename)
+            print(i)
 
-        instance_name_separated <- strsplit(instances_filenames[i][[1]][[1]], '[.]')[[1]]
+            instance_name_separated <- strsplit(instances_filenames[i][[1]][[1]], '[.]')[[1]]
 
-        instance_name_separated_by_slash <- strsplit(instance_name_separated, '/')[[1]]
+            print(instance_name_separated)
 
-        instance_name_separated_by_space <- strsplit(instance_name_separated, ' ')[[1]]
+            if(!(is.na(instance_name_separated) || instance_name_separated == '' || instance_name_separated == "  ")){
+                instance_name_separated_by_slash <- strsplit(instance_name_separated, '/')[[1]]
 
-        print(instance_name_separated_by_space)
+                instances_filename <- c()
+                index <- c()
 
-        instances_filename <- c()
-        index <- c()
-
-        if(length(instance_name_separated_by_slash) > 0){
-            instances_filename <- tail(instance_name_separated_by_slash,1)
-            print(instances_filename)            
-        }else if(length(instance_name_separated_by_space) > 0){
-            instances_filename <- tail(instance_name_separated_by_space,1)
-            print(instances_filename)   
-        }else{
-            instances_filename <- instance_name_separated[1]
-        }
-
-
-        split_by_colon <- strsplit(instance_name_separated[2], ':')[[1]]
-        extension <- c()
-
-        if(length(split_by_colon) > 0){
-            extension <- split_by_colon[1]
-        }else{
-            extension <- split_by_colon
-        }
-        
-        print('filename')
-        print(paste0(instances_filename, '.', extension))
-        files <- searchFile(paste0(instances_filename, '.', extension), here('benchmarks', 'instances'))
-        necesary_file <- c()
-
-        print('for///////////////////////////////////////////')
-        for(j in 1:length(files)){
-            if(length(grep('training', files[j])) > 0){
-                if(length(grep('training', head_filename)) > 0){
-                    necesary_file <- files[j]
+                if(length(instance_name_separated_by_slash) > 0){
+                    instances_filename <- tail(instance_name_separated_by_slash,1)
+                    print(paste('if slash', instances_filename))            
+                }else{
+                    instances_filename <- instance_name_separated[1]
                 }
-            }else if(length(grep('testing', files[j]) > 0)){
-                if(length(grep('testing', head_filename))){
-                    necesary_file <- files[j]
+
+
+                split_by_colon <- strsplit(instance_name_separated[2], ':')[[1]]
+                extension <- c()
+
+                if(length(split_by_colon) > 0){
+                    extension <- split_by_colon[1]
+                }else{
+                    extension <- split_by_colon
                 }
-            }else{
-                if(length(grep('trainning', head_filename)) == 0 && length(grep('testing', head_filename)) == 0){
-                    necesary_file <- files[j]
+
+                extension_separated_by_space <- strsplit(extension, " ")[[1]]
+
+                if(length(extension_separated_by_space) > 0){
+                    extension <- extension_separated_by_space[1]
+                }
+                
+                
+                if(is.na(extension)){
+                    files <- searchFile(instances_filename, here('benchmarks', 'instances'))
+                    print('na extension')
+                }
+                else{
+                    files <- searchFile(paste0(instances_filename, '.', extension), here('benchmarks', 'instances'))
+                }
+                necesary_file <- c()
+
+                print(paste('files', files))
+                print('for///////////////////////////////////////////')
+                for(j in 1:length(files)){
+                    if(length(grep('training', head_filename)) > 0){
+                        necesary_file <- files[j]
+                    
+                    }else if(length(grep('testing', head_filename))){
+                        necesary_file <- files[j]                
+                    }else{
+                        if(length(grep('trainning', head_filename)) == 0 && length(grep('testing', head_filename)) == 0){
+                            necesary_file <- files[j]
+                        }
+                    }
+                }
+                print(necesary_file)
+                if(length(necesary_file) == 0 || is.null(necesary_file) || is.na(necesary_file) || length(necesary_file) > 1){
+                    break
                 }
             }
-        }
-        print(necesary_file)
-        if(length(necesary_file) == 0 || is.null(necesary_file) || is.na(necesary_file) || length(necesary_file) > 1){
-            break
+
+            
         }
     }
 
